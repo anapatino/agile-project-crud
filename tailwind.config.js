6c77582ca236f24763de99ad96a3a5398a120682
Ana Patiño <79620046+anapatino@users.noreply.github.com>
2024-10-14 12:35:35 -0500

    installation of libraries and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,29 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
       colors: {
+        principal: "#F4F6FA",
+        primary: "#005FE3",
+        "gray-icon": "#9FA4AE",
+        "gray-text": "#6F7882",
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      screens: {
+        mb: "500px",
+        sm: "640px",
+        md: "768px",
+        lg1: "1040px",
+        lg: "1090px",
+        xl: "1280px",
+        "2xl": "1490px",
+        "3xl": "1700px",
+        "4xl": "2100px",
+        "h-sm": { raw: "(max-height: 700px)" },
+        "max-mb": { max: "500px" },
       },
     },
   },
